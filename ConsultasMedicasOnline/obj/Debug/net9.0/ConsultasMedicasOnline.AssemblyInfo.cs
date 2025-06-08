992c91a91cb113ddf3d4d67e69c63b8f5aad701b
Domingos-Daniel <domingoscahandadaniel@gmail.com>
2025-06-09 00:23:26 +0100

    feat: Add Editar Médico view with form validation and success/error messages

--- ConsultasMedicasOnline/obj/Debug/net9.0/ConsultasMedicasOnline.AssemblyInfo.cs
+++ ConsultasMedicasOnline/obj/Debug/net9.0/ConsultasMedicasOnline.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsultasMedicasOnline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89891dbfd6dec822fd5a21a4769d21cae7793bdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+781100e7cdc8e8665bf241f7ff6cd891a0504183")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsultasMedicasOnline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsultasMedicasOnline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
