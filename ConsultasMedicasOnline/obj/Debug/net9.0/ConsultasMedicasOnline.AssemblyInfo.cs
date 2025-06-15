5d2fdb5ba75fc7cf0ea91031f25623fe6f5b80c8
Domingos-Daniel <domingoscahandadaniel@gmail.com>
2025-06-15 20:58:44 +0100

    Refactor code structure for improved readability and maintainability

--- ConsultasMedicasOnline/obj/Debug/net9.0/ConsultasMedicasOnline.AssemblyInfo.cs
+++ ConsultasMedicasOnline/obj/Debug/net9.0/ConsultasMedicasOnline.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsultasMedicasOnline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cfae8449a3f41007b14a273358c6273cea7d40d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80e0e654afbe586fffdd75d69707e7d64bae6a81")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsultasMedicasOnline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsultasMedicasOnline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
